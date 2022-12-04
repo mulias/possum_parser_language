26940e66b7812e5ccb0fdf2d1d1b1029a5854959
Elias Mulhall <eli.mulhall@gmail.com>
2022-12-04 10:54:26 -0500

    Configure opam, dune-project, nix-shell to work together

--- opossum/shell.nix
+++ opossum/shell.nix
@@ -1,29 +1,13 @@
+{ pkgs ? import <nixpkgs> { } }:
+with pkgs;
+
 let
- pkgs = import <nixpkgs> {};
- # choose the ocaml version you want to use
- ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_13;
+ ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_14;
 in
 pkgs.mkShell {
-  # build tools
-  nativeBuildInputs = with ocamlPackages; [ ocaml findlib dune_3 ocaml-lsp ];
-  # dependencies
-  buildInputs = [
-    ocamlPackages.alcotest
-    ocamlPackages.angstrom
-    ocamlPackages.core
-    ocamlPackages.core_unix
-    ocamlPackages.ppx_deriving
-    ocamlPackages.ppx_let
-    ocamlPackages.ppx_string
-    ocamlPackages.re
-    ocamlPackages.utop
-    ocamlPackages.yojson
-    pkgs.ocamlformat
-  ];
-  # dune utop lib
-  # dune build
-  # dune exec ./bin/main.exe
+  buildInputs = [ pkgs.opam pkgs.pkg-config ocamlPackages.utop ocamlPackages.ocaml-lsp ];
   shellHook = ''
     export NVIM_OCAML_LSP=true
+    eval $(opam env)
   '';
 }
--- opossum/test/expect/json.t/run.t
+++ opossum/test/expect/json.t/run.t
@@ -12,8 +12,7 @@
             "Acronym": "SGML",
             "Abbrev": "ISO 8879:1986",
             "GlossDef": {
-              "para":
-                "A meta-markup language, used to create markup languages such as DocBook.",
+              "para": "A meta-markup language, used to create markup languages such as DocBook.",
               "GlossSeeAlso": [ "GML", "XML" ]
             },
             "GlossSee": "markup"
