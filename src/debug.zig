953fe1cdcf8e12c9a9082741342cb89bf61b0031
Elias Mulhall <eli.mulhall@gmail.com>
2024-06-03 11:48:44 -0400

    Remove unused debug print helpers
    We can't use these helpers any more because the cli and wasm apps get
    built at the some time. Even a temporary debug printing to stdout will
    fail to compile now.

--- src/debug.zig
+++ src/debug.zig
@@ -1,10 +1,2 @@
-const std = @import("std");
-
 pub const scanner = false;
 pub const parser = false;
-
-pub const writer = std.io.getStdErr().writer();
-
-pub fn print(comptime fmt: []const u8, args: anytype) void {
-    writer.print(fmt, args) catch return;
-}
