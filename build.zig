91d2b81cfee8185275347795d030d64bc6e472a0
Elias Mulhall <eli.mulhall@gmail.com>
2024-07-06 21:21:55 -0400

    Add release step to build.zig

--- build.zig
+++ build.zig
@@ -1,37 +1,61 @@
 const std = @import("std");
 const Build = std.Build;
+const Target = std.Target;
 const SemanticVersion = std.SemanticVersion;
+
+pub fn build(b: *Build) void {
+    const default_target = b.standardTargetOptions(.{});
+    const default_optimize = b.standardOptimizeOption(.{});
+
+    runStep(b, default_target, default_optimize);
+    testStep(b, default_target, default_optimize);
+    releaseStep(b);
+}
 
 const version: SemanticVersion = .{ .major = 0, .minor = 4, .patch = 0 };
 
-pub fn build(b: *Build) void {
-    const version_string = b.fmt("{d}.{d}.{d}", .{ version.major, version.minor, version.patch });
+fn versionString(b: *Build) []const u8 {
+    return b.fmt("{d}.{d}.{d}", .{ version.major, version.minor, version.patch });
+}
 
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    const library_opts = .{ .target = target, .optimize = optimize };
-
-    const wasm_target = b.resolveTargetQuery(.{
-        .cpu_arch = .wasm32,
-        .os_tag = .freestanding,
-    });
-
-    // Dependencies
-
-    const clap_module = b.dependency("clap", library_opts).module("clap");
-
-    // CLI
+fn addCliExecutable(b: *Build, name: []const u8, target: anytype, optimize: anytype) *Build.Step.Compile {
+    const clap_module = b.dependency("clap", .{
+        .target = target,
+        .optimize = optimize,
+    }).module("clap");
 
     const cli = b.addExecutable(.{
-        .name = "possum",
+        .name = name,
         .root_source_file = .{ .path = "src/cli.zig" },
         .target = target,
         .optimize = optimize,
     });
     cli.root_module.addImport("clap", clap_module);
 
-    cli.root_module.addAnonymousImport("docs/cli", .{
+    addDocImports(b, cli);
+    addBuildOptions(b, cli);
+
+    return cli;
+}
+
+fn addWasmExecutable(b: *Build, name: []const u8) *Build.Step.Compile {
+    const wasm = b.addExecutable(.{
+        .name = name,
+        .root_source_file = .{ .path = "src/wasm-lib.zig" },
+        .target = b.resolveTargetQuery(.{
+            .cpu_arch = .wasm32,
+            .os_tag = .freestanding,
+        }),
+        .optimize = .ReleaseSmall,
+    });
+    wasm.entry = .disabled;
+    wasm.rdynamic = true;
+
+    return wasm;
+}
+
+fn addDocImports(b: *Build, exe: *Build.Step.Compile) void {
+    exe.root_module.addAnonymousImport("docs/cli", .{
         .root_source_file = .{ .path = "docs/cli.txt" },
     });
 
@@ -47,39 +71,34 @@ pub fn build(b: *Build) void {
 
         const output = pandoc.captureStdOut();
 
-        cli.root_module.addAnonymousImport(output_name, .{
+        exe.root_module.addAnonymousImport(output_name, .{
             .root_source_file = output,
         });
     }
+}
 
-    const cli_options = b.addOptions();
-    cli.root_module.addOptions("build_options", cli_options);
-    cli_options.addOption([]const u8, "version", version_string);
+fn addBuildOptions(b: *Build, exe: *Build.Step.Compile) void {
+    const options = b.addOptions();
+    exe.root_module.addOptions("build_options", options);
+    options.addOption([]const u8, "version", versionString(b));
+}
+
+fn runStep(b: *Build, target: anytype, optimize: anytype) void {
+    const run_step = b.step("run", "Run CLI app");
+
+    const cli = addCliExecutable(b, "possum", target, optimize);
+
+    b.installArtifact(cli);
 
     const run_cli = b.addRunArtifact(cli);
     run_cli.step.dependOn(b.getInstallStep());
     if (b.args) |args| run_cli.addArgs(args);
 
-    const run_step = b.step("run", "Run the possum cli app");
     run_step.dependOn(&run_cli.step);
+}
 
-    b.installArtifact(cli);
-
-    // WASM
-
-    const wasm_lib = b.addExecutable(.{
-        .name = "possum",
-        .root_source_file = .{ .path = "src/wasm-lib.zig" },
-        .target = wasm_target,
-        .optimize = .ReleaseSmall,
-    });
-    wasm_lib.entry = .disabled;
-    wasm_lib.rdynamic = true;
-    wasm_lib.root_module.addImport("clap", clap_module);
-
-    b.installArtifact(wasm_lib);
-
-    // Tests
+fn testStep(b: *Build, target: anytype, optimize: anytype) void {
+    const test_step = b.step("test", "Run unit tests");
 
     const unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/tests.zig" },
@@ -89,6 +108,30 @@ pub fn build(b: *Build) void {
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
-    const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
+}
+
+fn releaseStep(b: *Build) void {
+    const release_step = b.step("release", "Build release binaries");
+
+    const wasm = addWasmExecutable(b, "possum");
+    const wasm_output = b.addInstallArtifact(wasm, .{});
+    release_step.dependOn(&wasm_output.step);
+
+    const targets: []const Target.Query = &.{
+        .{ .cpu_arch = .aarch64, .os_tag = .macos },
+        .{ .cpu_arch = .x86_64, .os_tag = .macos },
+        .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+        .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+    };
+
+    for (targets) |query| {
+        const target = b.resolveTargetQuery(query);
+        const target_string = query.zigTriple(b.allocator) catch @panic("OOM");
+        const name = b.fmt("possum_{s}", .{target_string});
+
+        const cli = addCliExecutable(b, name, target, .ReleaseSafe);
+        const target_output = b.addInstallArtifact(cli, .{});
+        release_step.dependOn(&target_output.step);
+    }
 }
