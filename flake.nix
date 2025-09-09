b25b6fd693086ece5c579ef40095ac3f6d38998c
Elias Mulhall <eli.mulhall@gmail.com>
2025-09-09 00:13:03 -0400

    use zls 0.15

--- flake.nix
+++ flake.nix
@@ -3,12 +3,13 @@
 
   inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-  inputs.zig.url = "github:mitchellh/zig-overlay";
+  inputs.zig-overlay.url = "github:mitchellh/zig-overlay";
+  inputs.zls-overlay.url = "github:zigtools/zls?ref=0.15.0";
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, zig }:
+  outputs = { self, nixpkgs, nixpkgs-unstable, zig-overlay, zls-overlay }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f (rec {
         pkgs = import nixpkgs {
           inherit system;
           config = {
@@ -21,14 +22,18 @@
             allowUnfree = true;
           };
         };
-      });
+        zig = zig-overlay.packages.${system}."0.15.1";
+        zls = zls-overlay.packages.${system}.zls.overrideAttrs (old: {
+          nativeBuildInputs = [ zig ];
+        });
+      }));
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs, unstable }: {
+      devShells = forEachSupportedSystem ({ pkgs, unstable, zig, zls }: {
         default = pkgs.mkShell {
           packages = [
-            zig.packages.${pkgs.system}."0.15.1"
-            unstable.zls
+            zig
+            zls
             pkgs.python311Packages.cram
             pkgs.pandoc
           ];
